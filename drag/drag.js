16787f77900401024b36a1ce596f22fa7bfd8c84
yousseffdil <60134214+yousseffdil@users.noreply.github.com>
2023-05-02 13:51:49 +0200

    GitHub Api

--- drag/drag.js
+++ drag/drag.js
@@ -4,40 +4,42 @@ let currentIcon = null;
 let initialX, initialY, currentX, currentY, xOffset = 0, yOffset = 0;
 
 icons.forEach(icon => {
-  icon.addEventListener('mousedown', dragStart);
+    icon.addEventListener('mousedown', dragStart);
 });
 
 window.addEventListener('mousemove', drag);
 window.addEventListener('mouseup', dragEnd);
 
 function dragStart(event) {
-  currentIcon = event.target.closest('.icon');
-  initialX = event.clientX - xOffset;
-  initialY = event.clientY - yOffset;
-  currentIcon.style = `
-  background: rgba(106, 96, 230, 0.59); border-radius: 5px; cursor: pointer;`
+    currentIcon = event.target.closest('.icon');
+    initialX = event.clientX - xOffset;
+    initialY = event.clientY - yOffset;
+
 
 }
 
 function drag(event) {
-  if (currentIcon) {
-    event.preventDefault();
-    currentX = event.clientX - initialX;
-    currentY = event.clientY - initialY;
+    if (currentIcon) {
+        event.preventDefault();
+        currentX = event.clientX - initialX;
+        currentY = event.clientY - initialY;
 
-    xOffset = currentX;
-    yOffset = currentY;
+        xOffset = currentX;
 
-    setTranslate(currentX, currentY, currentIcon);
-    
-  }
+        yOffset = currentY;
+
+        setTranslate(currentX, currentY, currentIcon);
+
+
+    }
 }
 
 function dragEnd(event) {
+
     currentIcon = null;
-  
+
 }
 
 function setTranslate(xPos, yPos, el) {
-  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
